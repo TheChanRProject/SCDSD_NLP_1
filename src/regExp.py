4ad1d4e868eb8cf9b81b98c253fe47efae82b6d8
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-19 08:39:54 -0700

    Adding edits

--- src/regExp.py
+++ src/regExp.py
@@ -4,6 +4,7 @@
 # r"(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?"
 # Facebook URL matching ^
 # https://regexr.com/
+# best reg exp for phone num: r"(\d{3}\D{0,3}\d{3}\D{0,3}\d{4})"
 
 import os
 import re
@@ -13,4 +14,5 @@ file = open("data/findNumbers.txt", "r")
 lines = file.readlines()
 for line in lines:
     print(line)
-    print(re.findall(r"(\d{3}\d{3}\d{4})", line))
+    print(re.findall(r"(\d{3}\D{0,3}\d{3}\D{0,3}\d{4})", line))
+    
