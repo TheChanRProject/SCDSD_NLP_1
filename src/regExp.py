842a9206285b7d73e9d908578bf932861a44ca88
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-19 08:27:14 -0700

    Adding edits

--- src/regExp.py
+++ src/regExp.py
@@ -1,3 +1,16 @@
 # r'[\w\.-]+@[\w\.-]+' Email
 # r"(\d{3}\d{3}\d{4})" Phone number based on digits
 # r".*?(\(?\d{3})? ?[\.-]? ?\d{3} ?[\.-]? ?\d{4}).*?" Phone number with possible delimiters
+# r"(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?"
+# Facebook URL matching ^
+# https://regexr.com/
+
+import os
+import re
+textFiles = os.listdir("data")
+
+file = open("data/findNumbers.txt", "r")
+lines = file.readlines()
+for line in lines:
+    print(line)
+    print(re.findall(r"(\d{3}\d{3}\d{4})", line))
